41e818f20c5ef5b6be29033f81df5f680c75fc3d
Joost van der Laan p <joost@j6.local>
2023-09-21 11:38:07 +0200

    fully working NixOS dev environment / VM

--- users/joost/nixos.nix
+++ users/joost/nixos.nix
@@ -15,7 +15,7 @@
     home = "/home/joost";
     extraGroups = [ "docker" "wheel" ];
     shell = pkgs.fish;
-    hashedPassword = "1ea9d598690f5ca5119b2a60b0f0f3b30b78deca60281e5eec938933de1e604b12e00d7987c1ff88a2fd5177d6659923e08a22065f75fe02bf8cd94c2f85bd25";
+    hashedPassword = "$6$nJOFfAkJl1RJMxUW$DuXpYNq7rc/TE7Awuyjv7vyOyzbUnHmxN3YN1Gz1DiAw363a9GkpEU6bU9MvYa94nXaP7oTSFbZegNb8kAcUm1";
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFB87It3cS6o8kgD/6r3R59KP2o1eOJz1bgLJl4syLX1 joost"
     ];
