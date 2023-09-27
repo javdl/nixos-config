e344f14ad04e6d19c44c7311bb4ab759d7e7fd7c
J <2742259+javdl@users.noreply.github.com>
2023-09-27 23:20:57 +0200

    working macos config with all Brew casks

--- users/joost/darwin.nix
+++ users/joost/darwin.nix
@@ -5,24 +5,51 @@
     (import ./vim.nix)
   ];
 
-  # homebrew = {
-  #   enable = true;
-  #   casks  = [
-  #     "1password"
-  #     "alfred"
-  #     "cleanshot"
-  #     "discord"
-  #     "google-chrome"
-  #     "hammerspoon"
-  #     "imageoptim"
-  #     "istat-menus"
-  #     "monodraw"
-  #     "rectangle"
-  #     "screenflow"
-  #     "slack"
-  #     "spotify"
-  #   ];
-  # };
+  homebrew = {
+    enable = true;
+    casks  = [
+      "alfred"
+      "authy"
+      "bitwarden"
+      "calibre"
+      "chromedriver"
+      "cleanshot"
+      "darktable"
+      "digikam"
+      "discord"
+      "docker"
+      "dropbox"
+      "element"
+      "figma"
+      "firefox"
+      "gimp"
+      "google-chrome"
+      "google-cloud-sdk"
+      "google-photos-backup-and-sync"
+      "gpg-suite"
+      "hammerspoon"
+      "imageoptim"
+      "inkscape"
+      "istat-menus"
+      "licecap"
+      "librewolf"
+      "libreoffice"
+      "monodraw"
+      "obs"
+      "postman"
+      "rapidapi"
+      "rectangle"
+      "screenflow"
+      "sequel-pro"
+      "slack"
+      "spotify"
+      "sublime-text"
+      "thunderbird"
+      "typora"
+      "visual-studio-code"
+      "vlc"
+    ];
+  };
 
   # The user should already exist, but we need to set this up so Nix knows
   # what our home directory is (https://github.com/LnL7/nix-darwin/issues/423).
