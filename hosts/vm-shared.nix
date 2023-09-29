2ab099d518a8d4ab17a8a25d57ca7c281fbd7e32
Joost van der Laan <joostvanderlaan@gmail.com>
2023-09-29 09:30:08 +0200

    set timezone

--- hosts/vm-shared.nix
+++ hosts/vm-shared.nix
@@ -44,7 +44,7 @@ in {
   networking.hostName = "dev";
 
   # Set your time zone.
-  time.timeZone = "America/Los_Angeles";
+  time.timeZone = "Europe/Amsterdam";
 
   # The global useDHCP flag is deprecated, therefore explicitly set to false here.
   # Per-interface useDHCP will be mandatory in the future, so this generated config
