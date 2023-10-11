ab212bce8c30f00d70fcd5efd9e6b7fe94c5b95a
Joost van der Laan <joostvanderlaan@gmail.com>
2023-10-11 15:16:26 +0200

    gnome settings, suspend, dark mode, night light

--- users/joost/home-manager.nix
+++ users/joost/home-manager.nix
@@ -95,6 +95,46 @@ in {
     "ghostty/config".text = builtins.readFile ./ghostty.linux;
   } else {});
 
+  # Gnome settings
+  # Use `dconf watch /` to track stateful changes you are doing, then set them here.
+  dconf.settings = {
+    "org/gnome/shell" = {
+      favorite-apps = [
+        "firefox.desktop"
+        "kgx.desktop"
+        "codium.desktop"
+        "org.gnome.Terminal.desktop"
+        #"spotify.desktop"
+        #"virt-manager.desktop"
+        "org.gnome.Nautilus.desktop"
+      ];
+    };
+    "org/gnome/desktop/interface" = {
+      color-scheme = "prefer-dark";
+      enable-hot-corners = false;
+    };
+    "org/gnome/desktop/wm/preferences" = {
+      workspace-names = [ "Main" ];
+    };
+    #"org/gnome/desktop/background" = {
+    #  picture-uri = "file:///run/current-system/sw/share/backgrounds/gnome/vnc-l.png";
+    #  picture-uri-dark = "file:///run/current-system/sw/share/backgrounds/gnome/vnc-d.png";
+    #};
+    #"org/gnome/desktop/screensaver" = {
+    #  picture-uri = "file:///run/current-system/sw/share/backgrounds/gnome/vnc-d.png";
+    #  primary-color = "#3465a4";
+    #  secondary-color = "#000000";
+    #};
+    "org/gnome/settings-daemon/plugins/color" = {
+      night-light-enabled = true;
+      night-light-schedule-automatic = true;
+    };
+    "org/gnome/settings-daemon/plugins/power" = {
+      sleep-inactive-ac-type = "nothing";
+      power-button-action = "interactive";
+    };
+  };
+
   #---------------------------------------------------------------------
   # Programs
   #---------------------------------------------------------------------
