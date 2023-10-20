85278173077985d20b4d9782296fb01624d652d9
Joost van der Laan <joostvanderlaan@gmail.com>
2023-10-20 16:30:34 +0200

    WIP cachix fu

---
+++ modules/cachix/fashionunited-public.nix
@@ -0,0 +1,12 @@
+{
+  nix = {
+    settings = {
+      substituters = [
+        "https://fashionunited-public.cachix.org"
+      ];
+      trusted-public-keys = [
+        "fashionunited-public.cachix.org-1:82RcZ6X7NjwUdX5bhPlqxtPLghl5peyIXFl+TfDLxlA="
+      ];
+    };
+  };
+}
