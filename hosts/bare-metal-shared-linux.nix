5b244e962b2fe602706052053c859e64e5efe0b8
Joost van der Laan <joostvanderlaan@gmail.com>
2023-12-06 12:50:17 +0100

    disable ML, enable thunderbolt

--- hosts/bare-metal-shared-linux.nix
+++ hosts/bare-metal-shared-linux.nix
@@ -27,6 +27,9 @@ in {
   '';
   security.polkit.enable = true;
 
+  # Thunderbolt. Devices might need to be enrolled: https://nixos.wiki/wiki/Thunderbolt
+  services.hardware.bolt.enable = true;
+
   nix = {
     # use unstable nix so we can access flakes
     package = pkgs.nixUnstable;
@@ -134,49 +137,49 @@ in {
     vscodium-fhs
     xclip
 
-    # ML
-    pciutils
-    file
-    git
-    gitRepo
-    gnupg
-    autoconf
-    curl
-    poetry
-    python311Packages.opencv4
-    # c
-    procps
-    gnumake
-    util-linux
-    m4
-    gperf
-    unzip
-    cudatoolkit
-    # cudaPackages.tensorrt
-    linuxPackages.nvidia_x11
-    libGLU
-    libGL
-    xorg.libXi
-    xorg.libXmu
-    freeglut
-    xorg.libXext
-    xorg.libX11
-    xorg.libXv
-    xorg.libXrandr
-    zlib
-    ncurses5
-    stdenv.cc
-    binutils
-    # ML libgl test fix
-    gcc13
-    SDL2
-    SDL2_ttf
-    SDL2_image
-    glfw
-    glew
-    # ML 3
-    # libstdcxx5
-    gcc
+    # # ML
+    # pciutils
+    # file
+    # git
+    # gitRepo
+    # gnupg
+    # autoconf
+    # curl
+    # poetry
+    # python311Packages.opencv4
+    # # c
+    # procps
+    # gnumake
+    # util-linux
+    # m4
+    # gperf
+    # unzip
+    # cudatoolkit
+    # # cudaPackages.tensorrt
+    # linuxPackages.nvidia_x11
+    # libGLU
+    # libGL
+    # xorg.libXi
+    # xorg.libXmu
+    # freeglut
+    # xorg.libXext
+    # xorg.libX11
+    # xorg.libXv
+    # xorg.libXrandr
+    # zlib
+    # ncurses5
+    # stdenv.cc
+    # binutils
+    # # ML libgl test fix
+    # gcc13
+    # SDL2
+    # SDL2_ttf
+    # SDL2_image
+    # glfw
+    # glew
+    # # ML 3
+    # # libstdcxx5
+    # gcc
 
 
 
