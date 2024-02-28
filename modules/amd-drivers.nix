78482f367ef73947b35a9335301c26ceb5bf636c
Joost van der Laan <joostvanderlaan@gmail.com>
2024-02-28 10:26:41 +0100

    extra AMD gpu settings

--- modules/amd-drivers.nix
+++ modules/amd-drivers.nix
@@ -9,4 +9,13 @@
 
     # Load AMD driver for Xorg and Wayland
     services.xserver.videoDrivers = ["amdgpu"];
+
+    # AMDVLK
+    hardware.opengl.extraPackages = with pkgs; [
+      amdvlk
+    ];
+    # For 32 bit applications 
+    hardware.opengl.extraPackages32 = with pkgs; [
+      driversi686Linux.amdvlk
+    ];
 }
