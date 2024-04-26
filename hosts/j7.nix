e387e433ae3096fb707d982d112230af47d08458
Joost van der Laan <joostvanderlaan@gmail.com>
2024-04-26 16:26:55 +0200

    remove conflicting settings

--- hosts/j7.nix
+++ hosts/j7.nix
@@ -71,17 +71,17 @@
   };
 
   # Enable the X11 windowing system.
-  services.xserver.enable = true;
+#  services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+#  services.xserver.displayManager.gdm.enable = true;
+#  services.xserver.desktopManager.gnome.enable = true;
 
   # Configure keymap in X11
-  services.xserver = {
-    layout = "us";
-    xkbVariant = "";
-  };
+#  services.xserver = {
+#    layout = "us";
+#    xkbVariant = "";
+#  };
 
   # Enable CUPS to print documents.
   services.printing.enable = true;
