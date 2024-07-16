81b4a9bb4b5e28db60e3ebb0d3e075e34626923c
Joost van der Laan <joost@fashionunited.com>
2024-07-16 12:40:36 +0200

    shared mac config

---
+++ hosts/mac-shared.nix
@@ -0,0 +1,72 @@
+{ config, pkgs, lib, currentSystem, currentSystemName,... }:
+
+let
+
+
+in {
+
+    # Keep in async with vm-shared.nix. (todo: pull this out into a file)
+    nix = {
+        # We need to enable flakes
+        extraOptions = ''
+            experimental-features = nix-command flakes
+            keep-outputs = true
+            keep-derivations = true
+        '';
+
+        # public binary cache that I use for all my derivations. You can keep
+        # this, use your own, or toss it. Its typically safe to use a binary cache
+        # since the data inside is checksummed.
+        settings = {
+            extra-substituters = ["https://javdl-nixos-config.cachix.org"];
+            trusted-public-keys = ["javdl-nixos-config.cachix.org-1:6xuHXHavvpdfBLQq+RzxDAMxhWkea0NaYvLtDssDJIU="];
+        };
+    };
+
+    # nix.trustedUsers = [
+    #     "@admin"
+    # ];
+
+    # # Auto upgrade nix package and the daemon service.
+    services.nix-daemon.enable = true;
+
+    # zsh is the default shell on Mac and we want to make sure that we're
+    # configuring the rc correctly with nix-darwin paths.
+    programs.zsh.enable = true;
+    programs.zsh.shellInit = ''
+      # Nix
+      if [ -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh' ]; then
+        . '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
+      fi
+      # End Nix
+      '';
+
+    programs.fish.enable = true;
+    programs.fish.shellInit = ''
+      # Nix
+      if test -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.fish'
+        source '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.fish'
+      end
+      # End Nix
+      '';
+
+    environment.shells = with pkgs; [ bashInteractive zsh fish ];
+    environment.systemPackages = with pkgs; [
+        cachix
+        kitty
+        terminal-notifier
+    ];
+
+    # https://github.com/nix-community/home-manager/issues/423
+    # environment.variables = {
+    #     TERMINFO_DIRS = "${pkgs.kitty.terminfo.outPath}/share/terminfo";
+    # };
+    programs.nix-index.enable = true;
+
+    # Keyboard
+    system.keyboard.enableKeyMapping = true;
+    system.keyboard.remapCapsLockToEscape = true;
+
+    # Add ability to used TouchID for sudo authentication
+    security.pam.enableSudoTouchIdAuth = true;
+}
--- hosts/macbook-pro-m1.nix
+++ hosts/macbook-pro-m1.nix
@@ -3,47 +3,9 @@
   # to manage it for us. This tells nix-darwin to just use whatever is running.
   nix.useDaemon = true;
 
-  # Keep in async with vm-shared.nix. (todo: pull this out into a file)
-  nix = {
-    # We need to enable flakes
-    extraOptions = ''
-      experimental-features = nix-command flakes
-      keep-outputs = true
-      keep-derivations = true
-    '';
+  imports =
+    [
+      ./mac-shared.nix
+    ];
 
-    # public binary cache that I use for all my derivations. You can keep
-    # this, use your own, or toss it. Its typically safe to use a binary cache
-    # since the data inside is checksummed.
-    settings = {
-      substituters = ["https://javdl-nixos-config.cachix.org"];
-      trusted-public-keys = ["javdl-nixos-config.cachix.org-1:6xuHXHavvpdfBLQq+RzxDAMxhWkea0NaYvLtDssDJIU="];
-    };
-  };
-
-  # zsh is the default shell on Mac and we want to make sure that we're
-  # configuring the rc correctly with nix-darwin paths.
-  programs.zsh.enable = true;
-  programs.zsh.shellInit = ''
-    # Nix
-    if [ -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh' ]; then
-      . '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
-    fi
-    # End Nix
-    '';
-
-  programs.fish.enable = true;
-  programs.fish.shellInit = ''
-    # Nix
-    if test -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.fish'
-      source '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.fish'
-    end
-    # End Nix
-    '';
-
-  environment.shells = with pkgs; [ bashInteractive zsh fish ];
-  environment.systemPackages = with pkgs; [
-    cachix
-    # zed-editor # Broken
-  ];
 }
