67234ce242ff6c3e3c1ff5a606ca2d198c119cf4
Joost van der Laan <joostvanderlaan@gmail.com>
2024-08-06 12:39:14 +0200

    fu137

--- hosts/fu137.nix
+++ hosts/fu137.nix
@@ -105,6 +105,7 @@
   environment.systemPackages = with pkgs; [
    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
    wget
+   chromium
    glib
     #github-runner
     gitlab-runner
