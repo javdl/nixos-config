1408632bc2768590c6cbf6acebd12e0cc0536954
J <2742259+javdl@users.noreply.github.com>
2025-01-06 16:33:53 +0100

    enable github runner

--- hosts/fu146.nix
+++ hosts/fu146.nix
@@ -13,4 +13,20 @@
     ];
 
     services.tailscale.enable = true;
+
+    services = {
+      github-runners = {
+        runner = {
+          enable = true;
+          name = "fu146-aarch64-darwin-runner";
+          # We suggest using the fine-grained PATs https://search.nixos.org/options?channel=24.05&show=services.github-runners.%3Cname%3E.tokenFile&from=0&size=50&sort=relevance&type=packages&query=services.github-runner
+          # The file should contain exactly one line with the token without any newline.
+          # https://github.com/settings/personal-access-tokens/new
+          # echo -n 'token' > ~/.fuww-github-runner-token
+          # Give it “Read and Write access to organization/repository self hosted runners”, depending on whether it is organization wide or per-repository.
+          tokenFile = "/home/joost/.fuww-github-runner-token";
+          url = "https://github.com/fuww";
+        };
+      };
+    };
 }
