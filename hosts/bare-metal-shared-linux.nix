aa13ba8c9e5a5f7c0e9a97586af66c9c8faf8477
Joost van der Laan <joostvanderlaan@gmail.com>
2025-01-25 13:47:16 +0100

    Try to fix VS Code Remote SSH

--- hosts/bare-metal-shared-linux.nix
+++ hosts/bare-metal-shared-linux.nix
@@ -203,13 +203,13 @@ in {
       ms-vscode-remote.remote-ssh
       vscode-icons-team.vscode-icons
 
-    ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
-      {
-        name = "remote-ssh-edit";
-        publisher = "ms-vscode-remote";
-        version = "0.47.2";
-        sha256 = "1hp6gjh4xp2m1xlm1jsdzxw9d8frkiidhph6nvl24d0h8z34w49g";
-      }
+#    ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+#      {
+#        name = "remote-ssh-edit";
+#        publisher = "ms-vscode-remote";
+#        version = "0.47.2";
+#        sha256 = "1hp6gjh4xp2m1xlm1jsdzxw9d8frkiidhph6nvl24d0h8z34w49g";
+#      }
     ];
   })
 
