d6a184f7812f197497fc742379f59277e1939b74
Joost van der Laan <joostvanderlaan@gmail.com>
2025-02-03 10:15:24 +0100

    update windsurf

--- pkgs/windsurf.nix
+++ pkgs/windsurf.nix
@@ -9,8 +9,8 @@
 
 # https://windsurf-stable.codeium.com/api/update/linux-x64/stable/latest
 let
-  version = "1.2.2"; # "windsurfVersion"
-  hash = "be4251dfb74e60e80fa973d61f3505da1ac9032e"; # "version"
+  version = "1.2.5"; # "windsurfVersion"
+  hash = "b195fa8f6708b2d32692f64ba2809ad303f79173"; # "version"
 in
   pkgs.callPackage "${pkgs.path}/pkgs/applications/editors/vscode/generic.nix" rec {
     inherit commandLineArgs useVSCodeRipgrep version;
@@ -23,7 +23,7 @@ in
 
     src = fetchurl {
       url = "https://windsurf-stable.codeiumdata.com/linux-x64/stable/${hash}/Windsurf-linux-x64-${version}.tar.gz";
-      hash = "sha256-s53azwr+bO7UHVAq0iydP09z7ZK9rvF2P7NKoGPmUMM="; # sha256 of b39ddacf0afe6ceed41d502ad22c9d3f4f73ed92bdaef1763fb34aa063e650c3
+      hash = "sha256-jIQX9NoH3CTSh8g6/RqB+gh8w/w0e9j8gH1TfCbvKqM="; # sha256 of b39ddacf0afe6ceed41d502ad22c9d3f4f73ed92bdaef1763fb34aa063e650c3
     };
 
     sourceRoot = "Windsurf";
