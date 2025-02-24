57bd3441784f5035b9ef0f54104c96fd5bf7b9bb
Joost van der Laan <joostvanderlaan@gmail.com>
2025-02-24 15:59:09 +0100

    renames

--- modules/specialization/plasma.nix
+++ modules/specialization/plasma.nix
@@ -1,11 +1,15 @@
 # KDE Plasma (Wayland)
 { pkgs, lib, ... }: {
   specialisation.plasma.configuration = {
-    services.xserver.enable = true;
-    services.displayManager = {
-      gdm.enable = lib.mkForce false;
-      sddm.enable = lib.mkForce true;
-      sddm.wayland.enable = true;
+    services.xserver = {
+      enable = true;
+      displayManager = {
+        gdm.enable = lib.mkForce false;
+        sddm = {
+          enable = lib.mkForce true;
+          wayland.enable = true;
+        };
+      };
     };
     services.desktopManager.plasma6.enable = true;
   };
