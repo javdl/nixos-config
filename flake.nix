eed47d0510a61a929aa47f6459b120b395c0c291
Joost van der Laan <joostvanderlaan@gmail.com>
2025-04-18 23:16:22 +0200

    nu repo

--- flake.nix
+++ flake.nix
@@ -83,6 +83,14 @@
       (final: prev: {
         # gh CLI on stable has bugs.
         gh = inputs.nixpkgs-unstable.legacyPackages.${prev.system}.gh;
+
+        # Want the latest version of these
+        nushell = inputs.nixpkgs-unstable.legacyPackages.${prev.system}.nushell;
+
+        # ibus = ibus_stable;
+        # ibus_stable = inputs.nixpkgs.legacyPackages.${prev.system}.ibus;
+        # ibus_1_5_29 = inputs.nixpkgs-old-ibus.legacyPackages.${prev.system}.ibus;
+        # ibus_1_5_31 = inputs.nixpkgs-unstable.legacyPackages.${prev.system}.ibus;
       })
     ];
 
