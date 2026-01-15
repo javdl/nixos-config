f4da6bc14e5f07c5aaf42a67002de5a377bdb977
Joost van der Laan <j@jlnw.nl>
2026-01-15 17:23:56 +0100

    Code checkpoint - 2026-01-15 17:23:56 (session: unknown)

--- flake.nix
+++ flake.nix
@@ -59,7 +59,23 @@
 
           # Want the latest version of these
           nushell = pkgs-unstable.nushell;
-        })
+        } // (if prev.stdenv.isDarwin then {
+          # Fix setproctitle test failures on macOS
+          python3 = prev.python3.override {
+            packageOverrides = pyFinal: pyPrev: {
+              setproctitle = pyPrev.setproctitle.overridePythonAttrs (old: {
+                doCheck = false;
+              });
+            };
+          };
+          python313 = prev.python313.override {
+            packageOverrides = pyFinal: pyPrev: {
+              setproctitle = pyPrev.setproctitle.overridePythonAttrs (old: {
+                doCheck = false;
+              });
+            };
+          };
+        } else {}))
     ];
 
     mkSystem = import ./lib/mksystem.nix {
