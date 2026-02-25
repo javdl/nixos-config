2da39c3e017c617f32566188998b929f4d4b63ba
Joost van der Laan <j@jlnw.nl>
2026-02-25 19:41:36 +0000

    upd

--- users/joost/nixos.nix
+++ users/joost/nixos.nix
@@ -2,13 +2,12 @@
 
 {
   # https://github.com/nix-community/home-manager/pull/2408
-  environment.pathsToLink = [ "/share/fish" "share/zsh" "share/nu" ];
+  environment.pathsToLink = [ "/share/zsh" "/share/fish" "/share/nu" ];
 
   # Add ~/.local/bin to PATH
   environment.localBinInPath = true;
 
-  # Since we're using fish as our shell
-  programs.fish.enable = true;
+  programs.zsh.enable = true;
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
@@ -16,7 +15,7 @@
     isNormalUser = true;
     home = "/home/joost";
     extraGroups = [ "docker" "wheel" ];
-    shell = pkgs.fish;
+    shell = pkgs.zsh;
     hashedPassword = "$6$nJOFfAkJl1RJMxUW$DuXpYNq7rc/TE7Awuyjv7vyOyzbUnHmxN3YN1Gz1DiAw363a9GkpEU6bU9MvYa94nXaP7oTSFbZegNb8kAcUm1";
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINAQwjDkpe7I8Y6xdD5SbICFy0v5ArILxyTBXhtSOOmw joostvanderlaan@gmail.com"
