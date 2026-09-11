aa78aca8c0b72113c12cb0086a293a5e447d8df1
Joost van der Laan <j@jlnw.nl>
2026-09-11 09:14:51 +0200

    feat: share Omarchy startup layout for fu137 and j9

--- users/joost/home-manager.nix
+++ users/joost/home-manager.nix
@@ -811,6 +811,29 @@ in
   );
 
   xdg.configFile = {
+    # Own only the requested login layout; Omarchy retains the rest of Hyprland.
+    "hypr/autostart.lua" =
+      lib.mkIf
+        (
+          isOmarchy
+          && builtins.elem currentSystemName [
+            "fu137"
+            "j9"
+          ]
+        )
+        {
+          force = true;
+          text = ''
+            -- Startup placement only: later windows open on the current workspace.
+            hl.on("hyprland.start", function()
+              hl.exec_cmd("/usr/bin/ghostty --gtk-single-instance=false -e ${pkgs.herdr}/bin/herdr --remote bali", { workspace = "1 silent" })
+              hl.exec_cmd("/usr/bin/chatgpt", { workspace = "2 silent" })
+              hl.exec_cmd("/usr/bin/zeditor --new", { workspace = "3 silent" })
+              hl.exec_cmd("/usr/bin/brave-origin", { workspace = "4 silent" })
+              hl.exec_cmd("/usr/bin/slack --gtk-version=3", { workspace = "5 silent" })
+            end)
+          '';
+        };
     "wezterm/wezterm.lua".text = ''
       local wezterm = require 'wezterm'
       local config = wezterm.config_builder()
